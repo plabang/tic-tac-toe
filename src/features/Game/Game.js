6519e5d917401dc28cc5094387b0eefd1b6299a8
Bang Pham <plabang.tma@gmail.com>
2021-06-03 16:07:03 +0700

    Add tic-tac-toe game

---
+++ src/features/Game/Game.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Board from "../../components/Board/Board";
+import './Game.css';
+
+
+class Game extends Component {
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Game;
